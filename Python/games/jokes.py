8619da87d82e008203395ec736428f7058188085
Brightbite <zindybiteme@gmail.com>
2018-03-17 17:16:14 +1100

    jokes

---
+++ Python/games/jokes.py
@@ -0,0 +1,23 @@
+#A joke telling program
+
+print('What do you get when you cross a snowman with a vampire?')
+guess = input()
+if guess == str('Frostbite'):
+    print('Correct!\n')
+else: print('Frostbite!\n')
+
+print('What do dentists call an astronaut\'s cavity?')
+guess = input()
+if guess == 'A black hole':
+    print('Correct!\n')
+else: print('A black hole!\n')
+
+print('Knock knock.')
+input()
+print("Who's there?")
+input()
+print('Interupting cow.')
+input()
+print('Interupting cow wh', end='')
+print('-MOO')
+
