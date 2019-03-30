a29ea53b5ebddd7459e88b0027ab29f8fcf24086
Brightbite <zindybiteme@gmail.com>
2019-03-31 01:44:24 +1100

    math functions

---
+++ Go/src/mymath/mymath.go
@@ -0,0 +1,26 @@
+package mymath
+
+func Add(a int, b int) int {
+	summ := a + b
+	return summ
+}
+
+func Sub(a int, b int) int {
+	summ := a - b
+	return summ
+}
+
+func Multiple(a int, b int) int {
+	summ := a * b
+	return summ
+}
+
+func Divide(a int, b int) int {
+	summ := a / b
+	return summ
+}
+
+func Remaining(a int, b int) int {
+	summ := a % b
+	return summ
+}
---
+++ Go/src/mypackage/reverse.go
@@ -0,0 +1,5 @@
+package mypackage
+
+func Reverse(s string) string {
+	return reverseTwo(s)
+}
